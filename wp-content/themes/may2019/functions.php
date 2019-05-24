2f2dc69773ff4760d659d37704dd8dba1b23e71b
Steve Persch <stevector@users.noreply.github.com>
2019-05-24 14:29:34 -0500

    Child theme (#2)
    * working on may2019 theme
    
    * Update style.css
    
    * Adding package.json
    
    * adding scss
    
    * adding a .gitignore
    
    * equeue
    
    * enqueue fix

---
+++ wp-content/themes/may2019/functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+wp_enqueue_style( 'custom_styles', get_stylesheet_directory_uri() . '/dist/styles.css');
