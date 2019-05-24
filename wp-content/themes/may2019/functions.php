243aa4704178946521dbaabc3328278b8fa681a4
Steve Persch <steve.persch@pantheon.io>
2019-05-24 13:42:28 -0500

    equeue

--- wp-content/themes/may2019/functions.php
+++ wp-content/themes/may2019/functions.php
@@ -3,3 +3,5 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+wp_enqueue_style( 'custom_styles', get_template_directory_uri() . '/dist/styles.css');
