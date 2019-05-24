6d7cc2d2fa991faafed5ce77d2bb82f2b618c90c
Steve Persch <steve@pantheon.me>
2019-05-24 19:18:28 +0000

    enqueue fix

--- wp-content/themes/may2019/functions.php
+++ wp-content/themes/may2019/functions.php
@@ -4,4 +4,4 @@ function enqueue_parent_styles() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-wp_enqueue_style( 'custom_styles', get_template_directory_uri() . '/dist/styles.css');
+wp_enqueue_style( 'custom_styles', get_stylesheet_directory_uri() . '/dist/styles.css');
